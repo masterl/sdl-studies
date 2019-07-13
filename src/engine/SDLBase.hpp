2adbf03cc2c6d8bb847045ebc81e0bd6bc75b2e1
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-07-12 21:54:56 -0700

    Added methods to retrieve sdl version (both compiled and linked)

--- src/engine/SDLBase.hpp
+++ src/engine/SDLBase.hpp
@@ -8,30 +8,13 @@
 class SDLBase
 {
     public:
-    SDLBase()
-    {
-        // SDL_INIT_TIMER      timer subsystem
-        // SDL_INIT_AUDIO      audio subsystem
-        // SDL_INIT_VIDEO      video subsystem;
-        //              automatically initializes the events subsystem
-        // SDL_INIT_JOYSTICK   joystick subsystem;
-        //              automatically initializes the events subsystem
-        // SDL_INIT_HAPTIC     haptic (force feedback) subsystem
-        // SDL_INIT_GAMECONTROLLER     controller subsystem;
-        //              automatically initializes the joystick subsystem
-        // SDL_INIT_EVENTS             events subsystem
-        // SDL_INIT_EVERYTHING         all of the above subsystems
-        // SDL_INIT_NOPARACHUTE        compatibility; this flag is ignored
-        if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO ) != 0 )
-        {
-            throw SDLException( "Unable to initialize SDL!" );
-        }
-    }
+    SDLBase();
+    ~SDLBase();
 
-    ~SDLBase()
-    {
-        SDL_Quit();
-    }
+    SDL_version version_compile_with() const;
+    SDL_version version_linked_to() const;
+
+    private:
 };
 
 #endif
